2d7f3b5b6dd99e22e1a53de04252ea78a953470c
pkhead <chromosoze@gmail.com>
2026-01-17 14:35:58 -0600

    1. adjust droplet horiz speed 2. add lake hint

--- root/game/consts.lua
+++ root/game/consts.lua
@@ -9,7 +9,7 @@ consts.COLGROUP_ENEMY   = 0x8
 consts.COLGROUP_ALL     = 0xFFFFFFFF
 
 consts.GRAVITY = 0.1
-consts.PLAYER_SIDE_SPIT_VX = 0.55
+consts.PLAYER_SIDE_SPIT_VX = 0.50
 consts.PLAYER_SIDE_SPIT_VY = -3.0
 consts.PLAYER_SIDE_SPIT_TARGET_Y_OFF = 5.0
 consts.PLAYER_SPIT_G_MULT = 2.0
@@ -17,6 +17,5 @@ consts.PLAYER_SPIT_G_MULT = 2.0
 consts.START_ROOM = "maps/b01.tmx"
 consts.RED_ORB_COUNT = 4
 consts.BLUE_ORB_COUNT = 3
--- TODO: orb count stats
 
 return consts
---
+++ root/res/dialogue/lake_hint.lua
@@ -0,0 +1,6 @@
+---@diagnostic disable
+say "Did you know? I'm a world-renowned writer, with prose that matches the likes of William Shakespeare and and Anton Chekhov. Here is a poem I made:"
+say "In that lake of sparkling blue /\nIn that large and spacious roo- /"
+say "m, There is more than meets the eyes. /\nObscured by darkness, its spaciousness hides. /"
+say "This poem's quality will make you cry /\nof laughter. Not of sad cry."
+say "Yay!!"
