59dd2e4820600d710e6f4f0e987a07adb0d44fb2
pkhead <chromosoze@gmail.com>
2026-01-17 12:01:01 -0600

    adjust some levels in response to playtest

---
+++ root/res/dialogue/room08_hint.lua
@@ -0,0 +1,5 @@
+---@diagnostic disable
+say "Did you know? If you jump up into a moveable block above you, the block will get bumped upwards."
+say "It's called the Law of Conservation, Newton's 7th Law. I think. I dunno I'm not a physician."
+say "But I don't think this information is very useful."
+say "Also if anyone knows how to get up that wall please tell me."
---
+++ root/res/dialogue/room09_hint.lua
@@ -0,0 +1,6 @@
+---@diagnostic disable
+say "Did you know?"
+say "..."
+say "Dangit, I forgot what I wanted to write. Sorry."
+say "Ugh, I can't seem to figure out how to bring that spring over here though. It would be useful."
+say "Anyway, sorry that I don't have any useful information... What a waste of wood."
