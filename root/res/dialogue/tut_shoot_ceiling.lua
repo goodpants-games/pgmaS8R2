8dcfe8ab1e208ae4a228d6cac23e2fb9f41a1c3b
pkhead <chromosoze@gmail.com>
2026-01-19 11:06:05 -0600

    update tut_shoot_ceiling dialogue

--- root/res/dialogue/tut_shoot_ceiling.lua
+++ root/res/dialogue/tut_shoot_ceiling.lua
@@ -1,3 +1,4 @@
 ---@diagnostic disable
-say "Did you know? You can shoot these ceiling-mounted enemies."
+say "Did you know? You can shoot these ceiling-mounted enemies from below."
+say "Just quickly go under them, shoot upwards, move out of the way, and repeat until they're down!"
 say "I hope this infor"
